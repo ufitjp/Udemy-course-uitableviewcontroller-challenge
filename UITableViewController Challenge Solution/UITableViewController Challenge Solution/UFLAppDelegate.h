827b60ca410c34f7e43102409fecf8c7edbfbd60
Prater, Jacob B <jacobp@ufl.edu>
2014-06-05 17:02:31 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/UITableViewController Challenge Solution/UFLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UFLAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Prater, Jacob B on 6/5/14.
+//  Copyright (c) 2014 Prater, Jacob B. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UFLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/UITableViewController Challenge Solution/UFLViewController.h
@@ -0,0 +1,13 @@
+//
+//  UFLViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Prater, Jacob B on 6/5/14.
+//  Copyright (c) 2014 Prater, Jacob B. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UFLViewController : UIViewController
+
+@end
